88032d48f83aee082fb751de0d1cebe05485c5f5
Mario Nunes <mariogar1979@gmail.com>
2024-05-18 00:19:08 -0300

    Adicionado um help para o run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,22 @@ LATEST_TAG="latest"
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
+show_help() {
+    echo "Uso: $0 [opções] [comando]"
+    echo
+    echo "Opções:"
+    echo "  --help, -h             Mostrar esta mensagem de ajuda e sair"
+    echo
+    echo "Comandos:"
+    echo "  Se nenhum comando for fornecido, um shell interativo (bash) será iniciado."
+    echo "  Qualquer comando fornecido será executado dentro do container Docker."
+    echo
+    echo "Exemplos:"
+    echo "  $0               Inicia um shell interativo (bash) dentro do container Docker."
+    echo "  $0 backup        Executa o script backup dentro do container Docker."
+}
+
+
 run() {
     if [ ! -d "home" ]; then
         mkdir -p home
@@ -23,6 +39,12 @@ run() {
         -e LOCAL_GROUP_ID=$GROUP_ID \
         $IMAGE_NAME:$LATEST_TAG "$@"
 }
+
+# Verificar se a opção de ajuda foi solicitada
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    show_help
+    exit 0
+fi
 
 # Executar a função 'run'
 run "$@"
