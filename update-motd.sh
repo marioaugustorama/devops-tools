9d66eba6ea7a76e6e7df91ec46376bf76660fe40
Mario Nunes <mariogar1979@gmail.com>
2024-05-27 22:55:14 -0300

    Ajustes no update-motd.sh

--- update-motd.sh
+++ update-motd.sh
@@ -3,8 +3,7 @@
 # Obter o IP da interface eth0 (ou substitua pela interface correta)
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
-# Atualizar o /etc/motd com o IP
-echo "********************************************" > /etc/motd
-echo "* Bem-vindo ao DevOps tools                *" >> /etc/motd
-echo "* IP da interface eth0: $IPADDRESS         *" >> /etc/motd
-echo "********************************************" >> /etc/motd
+echo "********************************************"
+echo "* Bem-vindo ao DevOps tools		  *"
+echo "* IP da interface eth0: $IP_ADDRESS	  *"
+echo "********************************************"
