ebd2170d223208511230afbe933c16b9376bc2f1
Mario Nunes <mariogar1979@gmail.com>
2024-08-19 23:45:22 -0300

    Ajustes diversos

--- update-motd.sh
+++ update-motd.sh
@@ -4,6 +4,6 @@
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 VERSION=$(cat /etc/version)
 echo "********************************************"
-echo "* Bem-vindo ao DevOps tools: $VERSION	  *"
+echo "* Bem-vindo ao DevOps tools: $APP_VERSION	  *"
 echo "* IP da interface eth0: $IP_ADDRESS	  *"
 echo "********************************************"
