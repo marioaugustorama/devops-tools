05ab0082a423e777500b09f34dc18be8b4633505
Mario Nunes <mariogar1979@gmail.com>
2024-08-19 23:42:08 -0300

    Diversos Ajustes aos scripts de instalação

--- scripts/mc.sh
+++ scripts/mc.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-curl -LO "https://dl.min.io/client/mc/release/linux-amd64/mc"
-install -o root -g root -m 0755 mc /usr/local/bin
-rm -rf mc
+# Inclui o arquivo com as funções genéricas
+source /usr/local/bin/utils.sh
+
+# Define a URL para o binário do MinIO Client (mc)
+MC_URL="https://dl.min.io/client/mc/release/linux-amd64/mc"
+MC_PATH="/usr/local/bin/mc"
+
+# Baixa o binário do MinIO Client
+echo "Baixando MinIO Client (mc)..."
+curl -sLO "$MC_URL" || error_exit "Falha ao baixar o MinIO Client"
+
+# Verifica se o arquivo foi baixado corretamente
+if [ ! -f "mc" ]; then
+    error_exit "O binário do MinIO Client não foi encontrado."
+fi
+
+# Instala o MinIO Client
+echo "Instalando MinIO Client (mc)..."
+install -o root -g root -m 0755 mc "$MC_PATH" || error_exit "Falha ao instalar o MinIO Client"
+
+# Limpeza
+echo "Limpando arquivos temporários..."
+rm -f mc || error_exit "Falha ao limpar arquivos temporários"
+
+echo "Instalação do MinIO Client (mc) concluída com sucesso."
--- scripts/opentofu.sh
+++ scripts/opentofu.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o /tmp/install-opentofu.sh
-chmod +x /tmp/install-opentofu.sh
-/tmp/install-opentofu.sh --install-method deb 
-rm -rf /tmp/install-opentofu.sh
+# Inclui o arquivo com as funções genéricas
+source /usr/local/bin/utils.sh
+
+# Define a URL para o script de instalação do OpenTofu
+INSTALL_SCRIPT_URL="https://get.opentofu.org/install-opentofu.sh"
+INSTALL_SCRIPT_PATH="/tmp/install-opentofu.sh"
+
+# Baixa o script de instalação
+echo "Baixando script de instalação do OpenTofu..."
+curl --proto '=https' --tlsv1.2 -fsSL "$INSTALL_SCRIPT_URL" -o "$INSTALL_SCRIPT_PATH" || error_exit "Falha ao baixar o script de instalação do OpenTofu"
+
+# Verifica se o script foi baixado corretamente
+if [ ! -f "$INSTALL_SCRIPT_PATH" ]; then
+    error_exit "O script de instalação $INSTALL_SCRIPT_PATH não foi encontrado."
+fi
+
+# Concede permissão de execução ao script
+echo "Concedendo permissão de execução ao script de instalação..."
+chmod +x "$INSTALL_SCRIPT_PATH" || error_exit "Falha ao conceder permissão de execução ao script de instalação"
+
+# Executa o script de instalação
+echo "Executando o script de instalação do OpenTofu..."
+"$INSTALL_SCRIPT_PATH" --install-method deb || error_exit "Falha ao instalar o OpenTofu"
+
+# Limpeza
+echo "Limpando arquivos temporários..."
+rm -rf "$INSTALL_SCRIPT_PATH" || error_exit "Falha ao limpar arquivos temporários"
+
+echo "Instalação do OpenTofu concluída com sucesso."
